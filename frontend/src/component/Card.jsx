079dd0d80a2f31f774fa5815aea0b2a9f5990e1b
shubhhamydv <shubhhamydv@gmail.com>
2026-06-21 08:39:40 +0530

    feat: implement course card UI

--- frontend/src/component/Card.jsx
+++ frontend/src/component/Card.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { FaStar } from "react-icons/fa6";
 
-function Card() {
+function Card({thumbnail, title, category, price, id}) {
   return (
-    <div className='max'>
+    <div className='max--sm w-full bg-white rounded-2xl overflow-hidden shadow-md hover:shadow-lg transition-all duration-300 border border-gray-300'>
+        <img src={thumbnail} alt="" className='w-full h-48 object-cover'/>
+        <div className='p-5 space-y-2'>
+            <h2 className='text-lg font-semibold text-gray-900'>
+                {title}
+                </h2>
+
+                <span className='px-2 py-0.5 bg-gray-100 rounded-full text-gray-700 capitalize'>
+                {category}
+                     </span>
+
+                     <div className='flex justify-between text-sm text-gray-600 mt-3 px-[10px]'>
+                        <span className='font-semibold text-gray-800'>{price}</span>
+                        <span className='flex items-center gap-1'> <FaStar className='text-yellow-500'/>5 </span>
+
+                     </div>
+
+            
+
+        </div>
       
     </div>
   )
